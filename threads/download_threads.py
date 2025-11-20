9aeb88289cad09b2ce0c47096a5bbee2ac681e63
Mrak408 <122750598+Mrak408@users.noreply.github.com>
2025-11-19 19:07:40 -0500

    Add 'Referer' header
    Myrient only removes the download throttle if this is present.

--- threads/download_threads.py
+++ threads/download_threads.py
@@ -399,7 +399,8 @@ class DownloadThread(QThread):
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive'
+            'Connection': 'keep-alive',
+            'Referer' : "https://myrient.erista.me/"
         }
 
         for i in range(self.retries):
